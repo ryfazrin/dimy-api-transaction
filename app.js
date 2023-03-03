52822840568d57e18f3d9444c9629b503b50a13e
ryfazrin <riansera515@gmail.com>
2023-03-03 14:17:53 +0700

    added: sql insert payment_method

--- app.js
+++ app.js
@@ -10,24 +10,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // create data / insert data
 app.post('/api/transaction', (req, res) => {
-  // buat variabel penampung data dan query sql
   const data = { ...req.body }
-  // console.log('data:', data)
-  // console.log('req.body: ', req.body)
-  // const querySql = 'INSERT INTO customer SET ?';
-
-  // // jalankan query
-  // let sqlOrder = `INSERT INTO order_transaction SET id=${connection.escape(data.id)}, customer_address=${connection.escape(data.customer_address)}, total=${connection.escape(data.total)}, order_date=${connection.escape(data.date)}`
-
-  // connection.query(sqlOrder, dataOrder, (err, rows, field) => {
-  //     // error handling
-  //     if (err) {
-  //         return res.status(500).json({ message: 'Gagal insert data!', error: err });
-  //     }
-
-  //     // jika request berhasil
-  //     res.status(201).json({ success: true, message: 'Berhasil insert data!' });
-  // });
 
   let sqlOrder = 'INSERT INTO order_transaction SET ?'
   let dataOrder = {
@@ -40,6 +23,7 @@ app.post('/api/transaction', (req, res) => {
   connection.beginTransaction(function (err) {
     if (err) { throw err; }
 
+    // insert order transaction
     const insertTransaction = new Promise((resolve, reject) => {
       connection.query(sqlOrder, dataOrder, (err, rows, field) => {
         // error handling
@@ -51,14 +35,15 @@ app.post('/api/transaction', (req, res) => {
           return res.status(500).json({ message: 'Gagal insert data!', error: err });
         }
 
-        resolve("Success!");
+        resolve(true);
       });
     });
 
+    // insert product to order_items
     insertTransaction.then(() => {
       let sqlOrderItems = 'INSERT INTO order_items SET ?'
 
-      let insertItems = data.order_items.map(product => {
+      data.order_items.map(product => {
         let dataItem = {
           order_transaction_id: data.id,
           product_id: product.product_id,
@@ -76,11 +61,32 @@ app.post('/api/transaction', (req, res) => {
           }
         });
       });
-
-      // if (insertItems) {
-      //   resolve("Success!");
-      // }
     })
+    // insert payment_method to order_payment_methods
+      .then(() => {
+
+        let sqlOrderPayMethods = 'INSERT INTO order_payment_methods SET ?'
+
+        data.order_payment_methods.map(payMethod => {
+          let dataPayMethod = {
+            order_transaction_id: data.id,
+            payment_method_id: payMethod.payment_method_id,
+            amount: payMethod.amount,
+            status: payMethod.status
+          }
+          connection.query(sqlOrderPayMethods, dataPayMethod, (err, rows, field) => {
+            // error handling
+            if (err) {
+              connection.rollback(function () {
+                throw err;
+              });
+
+              return res.status(500).json({ message: 'Gagal insert data!', error: err });
+            }
+          });
+        });
+      })
+    // commit trasaction sql
       .then(() => {
         connection.commit(function (err) {
           if (err) {
